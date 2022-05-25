c7d5dbb65efbe4b3820d15b4afb0a579dd0436ae
lucashlk8502 <lucashm767@gmail.com>
2022-05-25 14:12:18 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+
+
+
+
+function recebe(num){
+
+    var numero = document.querySelector(".btn").innerHTML= num
+ 
+
+   var text = `you select ${numero}`
+
+document.querySelector(".youselect").innerHTML= text
+
+}
+
+
+
+
+function enviar(){
+  
+
+    
+    
+    document.querySelector(".content2 ").style.marginRight= "550px"
+    
+    document.querySelector(".content2").style.display= "block"
+    
+document.querySelector(".content ").style.display= "none"
+
+
+
+
+    
+  
+
+
+   
+   
+
+
+}
